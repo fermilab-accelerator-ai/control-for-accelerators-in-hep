38c7500320127d9f9999315799d9de93e017831a
Malachi Schram <malachi.schram@pnnl.gov>
2020-05-31 17:12:24 -0700

    Register data accelerator environment

--- gym_accelerator/__init__.py
+++ gym_accelerator/__init__.py
@@ -1,10 +1,6 @@
 from gym.envs.registration import register
 
 register(
-    id='LSTM_Accelerator-v0',
-    entry_point='gym_accelerator.envs:LSTM_Accelerator',
-)
-register(
     id='Data_Accelerator-v0',
     entry_point='gym_accelerator.envs:Data_Accelerator',
 )
