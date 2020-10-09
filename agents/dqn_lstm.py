b67ddf9283206d12cb66d37c2146cb78b77f0191
Schram <schr476@we40963.pnl.gov>
2020-10-08 18:57:56 -0700

    Changes to the DQN LSTM model

--- agents/dqn_lstm.py
+++ agents/dqn_lstm.py
@@ -75,14 +75,14 @@ class DQN:
 
     def _build_model(self):
         model = Sequential()
-        model.add(LSTM(56, return_sequences=True,input_shape=(1, self.env.observation_space.shape[0])))
-        model.add(GaussianNoise(0.1))
-        model.add(LSTM(56, return_sequences=True))
-        model.add(GaussianNoise(0.1))
-        model.add(LSTM(56))
-        model.add(GaussianNoise(0.1))
+        model.add(LSTM(256, return_sequences=True,input_shape=(1, self.env.observation_space.shape[0])))
+        #model.add(GaussianNoise(0.1))
+        model.add(LSTM(256, return_sequences=True))
+        #model.add(GaussianNoise(0.1))
+        model.add(LSTM(256))
+        #model.add(GaussianNoise(0.1))
         model.add(Dense(self.env.action_space.n,))
-        opt = Adam(lr=1e-4)
+        opt = Adam(lr=1e-2)
         model.compile(loss='mean_squared_error', optimizer=opt)
         model.summary()
         return model       
